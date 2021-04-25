fddf42b721397858afcac11193bf2c83f9c67aa6
atticusg <atticusg@stanford.edu>
2021-04-25 01:22:55 -0700

    Update MLP

--- antra/compgraphs/MLP.py
+++ antra/compgraphs/MLP.py
@@ -11,21 +11,16 @@ def generate_MLP_compgraph(MLP, activation, output_function = lambda x:x):
     """
     input_leaf = GraphNode.leaf("input")
     curr_node = GraphNode(input_leaf,name ="hidden_layer_1" ,forward=lambda x: activation(torch.matmul(x,torch.from_numpy(MLP.coefs_[0])) + torch.from_numpy(MLP.intercepts_[0])))
-    name = "hidden_layer_2"
-    function = lambda x: activation(torch.matmul(x,torch.from_numpy(MLP.coefs_[1])) + torch.from_numpy(MLP.intercepts_[1]))
-    curr_node2 = GraphNode(curr_node,name=name,forward=function)
-    name = "hidden_layer_3"
-    function = lambda x: activation(torch.matmul(x,torch.from_numpy(MLP.coefs_[2])) + torch.from_numpy(MLP.intercepts_[2]))
-    curr_node3 = GraphNode(curr_node2,name=name,forward=function)
-    # for i in range(1,len(MLP.coefs_)):
-    #     name = "hidden_layer_" + str(i+1)
-    #     function = lambda x: activation(torch.matmul(x,torch.from_numpy(MLP.coefs_[i])) + torch.from_numpy(MLP.intercepts_[i]))
-    #     curr_node = GraphNode(curr_node,name=name,forward=function)
+    for i in range(1, MLP.n_layers_-1):
+        name = "hidden_layer_" + str(i+1)
+        f = lambda x: activation(torch.matmul(x,torch.from_numpy(MLP.coefs_[i])) + torch.from_numpy(MLP.intercepts_[i]))
+        curr_node2 = GraphNode(curr_node,name=name,forward=f)
+        curr_node = curr_node2
     if MLP.out_activation_ == "identity":
-        output = GraphNode(curr_node3,name="output",forward=lambda x: x)
+        output = GraphNode(curr_node,name="output",forward=lambda x: x)
     elif MLP.out_activation_ == "logistic":
-        output = GraphNode(curr_node3,name="output",forward=sigmoid)
+        output = GraphNode(curr_node,name="output",forward=sigmoid)
     elif MLP.out_activation_ == "softmax":
-        output = GraphNode(curr_node3,name="outputt",forward=lambda x: softmax(x,dim=0))
+        output = GraphNode(curr_node,name="outputt",forward=lambda x: softmax(x,dim=0))
     root = GraphNode(output,name="root",forward=output_function)
     return ComputationGraph(root)
