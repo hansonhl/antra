3f97c1f287d8cb6b84fb817e54e25d31fc66f803
atticusg <atticusg@stanford.edu>
2021-04-26 16:14:09 -0700

    Update generate_MLP to work for the arbitrary case

--- antra/compgraphs/MLP.py
+++ antra/compgraphs/MLP.py
@@ -5,17 +5,29 @@ from torch.nn.functional import softmax, sigmoid
 from antra import ComputationGraph, GraphNode, GraphInput
 from antra.utils import serialize
 
+def _generate_MLP_layer_function(activation, MLP, i):
+    """ Generate a function for a layer in bert.
+    Each layer function takes in a previous layer's hidden states and outputs,
+    and only returns the hidden states.
+
+    :param layer_module:
+    :param i:
+    :return: Callable function that corresponds to a bert layer
+    """
+    def _MLP_layer_function(x):
+        return activation(torch.matmul(x,torch.from_numpy(MLP.coefs_[i])) + torch.from_numpy(MLP.intercepts_[i]))
+    return _MLP_layer_function
+
 def generate_MLP_compgraph(MLP, activation, output_function = lambda x:x):
     """
     Converts an sklearn MLP Classifier to a comp graph.
     """
     input_leaf = GraphNode.leaf("input")
     curr_node = GraphNode(input_leaf,name ="hidden_layer_1" ,forward=lambda x: activation(torch.matmul(x,torch.from_numpy(MLP.coefs_[0])) + torch.from_numpy(MLP.intercepts_[0])))
-    for i in range(1, MLP.n_layers_-1):
+    for i in range(1,len(MLP.coefs_)):
         name = "hidden_layer_" + str(i+1)
-        f = lambda x: activation(torch.matmul(x,torch.from_numpy(MLP.coefs_[i])) + torch.from_numpy(MLP.intercepts_[i]))
-        curr_node2 = GraphNode(curr_node,name=name,forward=f)
-        curr_node = curr_node2
+        function = _generate_MLP_layer_function(activation, MLP, i)
+        curr_node = GraphNode(curr_node,name=name,forward=function)
     if MLP.out_activation_ == "identity":
         output = GraphNode(curr_node,name="output",forward=lambda x: x)
     elif MLP.out_activation_ == "logistic":
